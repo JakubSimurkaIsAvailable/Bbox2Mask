2b5bc5938713ccb16fcf42b9167d2ed8fdd0a712
JakubSimurkaIsAvailable <simurka.jakub@gmail.com>
2025-08-01 14:27:15 +0200

    added processing of image

--- backend.py
+++ backend.py
@@ -2,30 +2,38 @@ import numpy as np
 import cv2
 import torch
 import matplotlib.pyplot as plt
+from segment_anything import sam_model_registry, SamPredictor
+from segment_anything.utils.transforms import ResizeLongestSide
 
 
 
-#wont need >:3
-def show_mask(mask, ax, random_color=False):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
+def load_image(imageurl):
+    global image_original
+    image_original = cv2.imread(imageurl)
     
-def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+def load_sam():
+    global predictorSam, sam
     
-def show_box(box, ax):
-    x0, y0 = box[0], box[1]
-    w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+    sam_checkpoint = "sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+    device = "cuda"
     
+    sam = sam_model_registry[model_type](checkpoint = sam_checkpoint)
+    sam.to(device = device)
+    predictorSam = SamPredictor(sam)
 
-def image_processor():
-    image = cv2.imread()
+def process_image(bboxes):
+    image_boxes = torch.tensor(bboxes, device=sam.device)
+    resize_transform = ResizeLongestSide(sam.image_encoder.img_size)
+    batched_input = {
+        'image': prepare_image(image_original, resize_transform, sam),
+        'boxes': resize_transform.apply_boxes_torch(image_boxes, image_original.shape[:2]),
+        'original_size': image_original.shape[:2]
+    }
+    batched_output = sam(batched_input, multimask_output = False)
+    return batched_output[0]['masks']
+    
+def prepare_image(image, transform, device):
+    image = transform.apply_image(image)
+    image = torch.as_tensor(image, device=device.device)
+    return image.permute(2, 0, 1).contiguous()
