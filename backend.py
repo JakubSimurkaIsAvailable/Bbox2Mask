14650e89156eceef6c95baf8ac11edf17cb0db4e
JakubSimurkaIsAvailable <simurka.jakub@gmail.com>
2025-08-01 15:53:51 +0200

    comments and minor fixes

--- backend.py
+++ backend.py
@@ -5,12 +5,19 @@ import matplotlib.pyplot as plt
 from segment_anything import sam_model_registry, SamPredictor
 from segment_anything.utils.transforms import ResizeLongestSide
 
-
+loaded_images = []
+loaded_images_boxes = []
+loaded_images_masks = []
 
 def load_image(imageurl):
     global image_original
     image_original = cv2.imread(imageurl)
+    loaded_images.append(image_original)
     
+def load_boxes(boxes):
+    loaded_images_boxes.append(boxes)
+    
+    #sets up segment anything model
 def load_sam():
     global predictorSam, sam
     
@@ -22,18 +29,23 @@ def load_sam():
     sam.to(device = device)
     predictorSam = SamPredictor(sam)
 
-def process_image(bboxes):
+    #gets the image and bboxes adds masks to the list
+def process_image(bboxes, image_p):
     image_boxes = torch.tensor(bboxes, device=sam.device)
     resize_transform = ResizeLongestSide(sam.image_encoder.img_size)
     batched_input = {
-        'image': prepare_image(image_original, resize_transform, sam),
-        'boxes': resize_transform.apply_boxes_torch(image_boxes, image_original.shape[:2]),
-        'original_size': image_original.shape[:2]
+        'image': prepare_image(image_p, resize_transform, sam),
+        'boxes': resize_transform.apply_boxes_torch(image_boxes, image_p.shape[:2]),
+        'original_size': image_p.shape[:2]
     }
     batched_output = sam(batched_input, multimask_output = False)
-    return batched_output[0]['masks']
+    loaded_images_masks.append(batched_output[0]['masks'])
     
+    #helper function to transform image
 def prepare_image(image, transform, device):
     image = transform.apply_image(image)
     image = torch.as_tensor(image, device=device.device)
     return image.permute(2, 0, 1).contiguous()
+
+#TODO add method to create an annotation for the image in a specific format
+#TODO convert images to their correct format
